5d40452840b54c9e22061596aa424e2a658bd707
AngelaFlorez <amflorez80793@umanizales.edu.co>
2021-03-11 18:59:09 -0500

    Adiciona la clase vehiculo carga

--- Laboratorio/src/modelo/Vehiculo.java
+++ Laboratorio/src/modelo/Vehiculo.java
@@ -14,6 +14,7 @@ public class Vehiculo {
     String matricula;
     String marca;
     
+   
     public Vehiculo (String matricula, String marca ){
        this.matricula = matricula;
        this.marca = marca;

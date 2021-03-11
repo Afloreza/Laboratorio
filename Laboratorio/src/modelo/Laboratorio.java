d792df6570cf39a0cf865646f837e72f13dc7cd8
AngelaFlorez <amflorez80793@umanizales.edu.co>
2021-03-11 18:53:58 -0500

    Se crea dos vehiculos adicionales en la clase principal

--- Laboratorio/src/modelo/Laboratorio.java
+++ Laboratorio/src/modelo/Laboratorio.java
@@ -14,12 +14,15 @@ public class Laboratorio {
 
     public static void main(String[] args) {
      
-        Vehiculo vehiculos [] = new Vehiculo [4];
+        Vehiculo vehiculos [] = new Vehiculo [6];
         
         vehiculos[0]= new Vehiculo("H7GRV1","Toyota");
         vehiculos[1] = new vehiculoTransporte(25, 6, "Y87HDB", "Toyota");
         vehiculos[2] = new vehiculoTransporte(21, 4, "I09UDE", "Mercedes");
         vehiculos[3] = new VehiculoCompetencia(200, "Y87HDB", "Toyota");
+        vehiculos[4] = new VehiculoCompetencia(200, "U5H5TH", "Ford");
+        vehiculos[5] = new VehiculoCompetencia(200, "86BKY6", "Ferrari");
+        
         
         for (Vehiculo vehiculo1: vehiculos){
         
